7e84acde182a245abaa40caad26e141a708c16cb
ankush-vapta <vapta.ankush@gmail.com>
2024-05-21 13:33:25 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Vincertax from "./component/HomeComponent/Vincertax";
 import Login from "./component/Login/Login";
+import { Workspacehome } from './component/workspace/Workspacehome'
 function App() {
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<Vincertax />} />
-          <Route path="/Login" element={<Login/>} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="workspacehome" element={<Workspacehome />} />
           <Route path="/NotFound" element={<>NotFound</>} />
         </Routes>
       </Router>
